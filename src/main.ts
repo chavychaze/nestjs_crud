72ee338e1fe960e91254b4278b1a7f6d371d1584
v_sobol <sobolv@mydigicode.com>
2019-09-13 16:39:02 +0300

    add Swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LotocModule } from './lotoc/lotoc.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Lotoc routes')
+    .setDescription('The lotoc API description')
+    .setVersion('1.0')
+    .addTag('Lotoc integration')
+    .build();
+  const lotocDocument = SwaggerModule.createDocument(app, options, {
+    include: [LotocModule]
+  });
+  SwaggerModule.setup('swagger/lotoc', app, lotocDocument);
+
+//   const secondOptions = new DocumentBuilder()
+//   .setTitle('Second routes')
+//   .setDescription('The second API description')
+//   .setVersion('1.0')
+//   .addTag('Second integration')
+//   .build();
+// const secondDocument = SwaggerModule.createDocument(app, options, {
+//   include: [SecondModule]
+// });
+// SwaggerModule.setup('swagger/second', app, secondDocument);
+
   await app.listen(3000);
 }
 bootstrap();
